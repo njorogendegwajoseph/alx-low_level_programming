35d610311d6e925554f2c6c3b40091318a329f3f
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 03:51:12 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  *function: main - will print the words in bracket
  * return:  0 means the expected value is an integer, a zero and cide ran well
  */
-
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle\n");
