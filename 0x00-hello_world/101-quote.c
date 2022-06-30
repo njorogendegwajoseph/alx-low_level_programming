ce1cc636292b67179bca0e2168cf39fc1f6e56e7
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 09:27:41 -0700

    Hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main-This is the entry point
  *
- * Return - a  function that returns 1 Meaning success.
- */
-int main()
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 1 (Success)
+ *
+ *     */
+
+int main(void)
+
 {
-	char str1[] = "and that piece of art is beautiful\" -Dora Korpar, 2015-10-19\n";
-	
-	write(2, str1, 59);
-	return (1);
+
+		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
+
+			write(2, str1, 59);
+
+				return (1);
+
 }
