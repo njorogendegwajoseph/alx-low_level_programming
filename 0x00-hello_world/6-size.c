cae93e8bc7fc75b6a8ef6d59a722e745c07cc08b
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 09:21:34 -0700

    Much

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 /**
- * This code displays the sizes of different data types
- * Main- This is a function.
- * Description - This function initiates the comiling process.
- * Return _ This is the return function.
- * Description _ This function prints returns 0 on succesful completion.
+ * Main- This is a function that initializes compilation.
+ *
+ * Return -  This returns 0 when succesful
  */
 int main(void)
 {
-		printf("size of int: %d bytes\n", sizeof(int));
-		printf("size of a long int: %d bytes\n", sizeof(long int));
-		printf("Size of a float:%d bytes\n", sizeof(float));
-		printf("Size of a char: %d bytes\n", sizeof(char));
-		printf("Size of a long long int: %d bytes", sizeof(long long int));
+		printf("size of int: %d byte(s)\n", sizeof(int));
+		printf("size of a long int: %d byte(s)\n", sizeof(long int));
+		printf("Size of a float:%d byte(s)\n", sizeof(float));
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+		printf("Size of a long long int: %d byte(s)", sizeof(long long int));
 		return (0);
 }
