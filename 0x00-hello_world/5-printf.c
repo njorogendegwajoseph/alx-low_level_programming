d4b6db7eea50c7e97cdfdee2a651bd261c423ccd
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 08:59:44 -0700

    Hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * Function main- The main function initiates the compiling process, and printsi words in bracket. 
- * return function - returns zero on succesful coode.
+ * main- The main function initiates the coimpiling process, and printsi words in bracket.
+ *
+ * return function - returns 0 on succesful code.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
