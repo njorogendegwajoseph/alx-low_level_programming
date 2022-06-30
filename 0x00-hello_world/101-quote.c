6784882987c2b7568a136e283e932c3b287467c9
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 07:53:30 -0700

    Hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main-prints what is contained in the parenthesis.
+ * followed by a new line as specified in the task
+ * return-always rturn 0 if code is succesful.
+ */
+int main()
+{
+	write(2, "and that piece of art is beautiful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
