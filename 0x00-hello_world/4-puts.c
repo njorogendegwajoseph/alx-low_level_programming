b444ed2b07e38c06780948fdec460f6ffa81e026
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 01:31:56 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main will print the words in bracket
- * int means the expected value is an integer, a zero
- * return 0 means the execution ran well.
+ * main - will print the words in bracket
+ * int: means the expected value is an integer, a zero
+ * return: 0 means the execution ran well.
  */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
  
