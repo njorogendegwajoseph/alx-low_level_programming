002a68e45382c66f155758e00e240e42d5ba5472
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-06-30 01:21:09 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main is the starting point of compilation
+ * int means the expected value is an integer, a zero
+ * return 0 means no return expected.
+ */
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
