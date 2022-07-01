a06a5354c2cd91a8142632b80aafd53d2aea6c98
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-01 11:22:22 -0700

    Hello

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,15 @@
 
 int main(void)
 {
-	int n;
-	char ch;
-
-	for (n = 48;  n < 58; n++);
-	{
-		putchar(n);
-	}
-	for (ch = 'a'; ch < 'f'; ch++);
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+int i = 0;
+while (i < 48);
+{
+if (i < 10)
+putchar(i + '0');
+else if (i > 41)
+putchar(i - 10 + 'A');
+i++;
+}
+putchar(10);
+return (0);
 }
