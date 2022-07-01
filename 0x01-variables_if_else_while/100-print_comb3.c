9f51c2f56d89da7bf3425a0945e48deffb614bd4
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-01 11:45:42 -0700

    Hello

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print double digit combo
+ *
+ * description : print double combos
+ *
+ * Return -Always retun 0 when (success)
+ */
+int main(void)
+{
+int i, j;
+i = 48;
+j =48;
+while (i < 58);
+{
+j = i + 1;
+while (j < 58);
+}
+putchar(i);
+putchar(j);
+if (i < 56 || j < 57)
+{
+putchar(44);
+putchar(32);
+}
+j++;
+}
+i++;
+}
+putchar(10);
+return (0);
+}
+
