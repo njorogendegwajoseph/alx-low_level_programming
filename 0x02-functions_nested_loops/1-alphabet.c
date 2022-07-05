dc34c314621590ef1236dfeee75b846589bac142
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-05 00:45:43 -0700

    Hello now

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main- prints the alphabets in lower case
+ *
+ * return - returns 0 when succesful
+ *
+ */
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+}
