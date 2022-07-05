f47e2cb0f53bbe9f663158c7b341f5b7fa37b1f9
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-05 10:04:49 -0700

    Hello

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * main - Computes the actual value of an integer
- * returns 0 when succesful
+ * returns n succesful.
  *
  */
 int _abs(int n)
 {
 if (n >= 0)
 return (n);
+}
+return (-n);
 }
