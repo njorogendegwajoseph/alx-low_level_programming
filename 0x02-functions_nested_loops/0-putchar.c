f5dc349a9e95a76ea703c8351575af3ff342bba7
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-05 00:36:47 -0700

    Another day

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is the initializing point of the program
+ *
+ * return 0 when succesfful.
+ */
+
+int main(void)
+{
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar{\n);
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is fr functions and nested loops only
