42687a981cdd9f32ec56f96897240686b683accf
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-05 01:11:54 -0700

    Hello

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <main.h>
+
+/**
+ * main - print sthe lower cas letters
+ * returns 1 when c is lower case
+ * returns 0 if otherwise
+ *
+ */
+int is_lower(int c)
+{
+return (c >= 'a' && <= 'z');
+}
