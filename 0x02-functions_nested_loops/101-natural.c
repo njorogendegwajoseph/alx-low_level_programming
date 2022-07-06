bd9f6f76f5b173c9c46ec355477adcf186fea7bf
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-06 00:07:28 -0700

    Hello

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -19,10 +19,10 @@ int main(void)
 	{ 
 		if ((i % 3) == 0)
 		{
-			sum3 = sum3 + 1;
+			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum5 = sum5 + 1;
+			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
