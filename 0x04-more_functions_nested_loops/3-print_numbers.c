b00066527774ac472f121d9b904f112017ca88f0
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-07 02:26:06 -0700

    Hello

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * print _numbers : this will print numbers from 0-9
  * return: this will return 0 when succesful.
  */
 void print_numbers(void)
 {
-	int a;
+	int n;
 
-	a = 0;
-	while (a >= 0 && a <= 9)
-		_putchar("%d\n", a);
-	a++;
+	for (n = '0' ; n <= '9'; n++)
+		_putchar(n);
+	_putchar('\n');
 }
+
