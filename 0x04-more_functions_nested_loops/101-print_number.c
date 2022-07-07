c02b5da35c6b128bc8e66c5709982d7ec9cc4c53
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-07 05:13:39 -0700

    Hello

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,20 +6,20 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n = -n;
 		_putchar('_');
 	} else
 	{
-		n1 = n;
+		n = n;
 	}
 
-	if (n1 / 10)
+	if (n / 10)
 	{
-		print_number(n1 / 10);
+		print_number(n / 10);
 	}
-	_putchar((n1 %1 10) + '0');
+	_putchar((n %1 10) + '0');
 }
