2dcd3c2ceeb2da27246d5207bcea0f5c2b814b84
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-11 03:33:00 -0700

    Hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strlen: will return string length
+ * return 0;
+ */
+
+int strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	retur (length);
+}
+
