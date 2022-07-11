e5a552b72367d8eff2d4f0f47c31afd160c6ebe5
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-11 07:03:24 -0700

    Hello

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-
 int _putchar(char c);
 
 void swap_int(int *a, int *b);
@@ -23,5 +19,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
-#endif
 
