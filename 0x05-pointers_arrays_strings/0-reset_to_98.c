8ffa1b417829c7f913eb6d6174c9ae40444bf2b4
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-11 03:08:44 -0700

    Hello

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,28 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98: This will reset to 98. and is the  entry.
- * @n : print the value
- * return : 0;
+ * rest_to_98: entry point
+ * @n: prin the value
+ * return: 0
  */
 void reset_to_98(int *n)
 {
-	int *n = 98;
-}#include "main.h"
-/**
- * reset_to_98: This will reset to 98. and is the  entry.
- * @n : print the value
- * return : 0;
- */
-void reset_to_98(int *n)
-{
-	int *n = 98;
-}#include "main.h"
-/**
- * reset_to_98: This will reset to 98. and is the  entry.
- * @n : print the value
- * return : 0;
- */
-void reset_to_98(int *n)
-{
-	int *n = 98;
+	*n = 98:
 }
