1d6050f457d03bacf803f9d1e6d53440ebdbdfe5
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-11 06:27:18 -0700

    Hello

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	i = 0;
+
+	while (i < n)
+	{
+	printf("%d", a[i]);
+	if (i < n - 1)
+	{
+		printf(", ");
+	}
+	i++
+	}
+	printf("\n");
+}
