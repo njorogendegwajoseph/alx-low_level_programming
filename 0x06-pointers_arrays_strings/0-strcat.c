a9f8ae34e5d4a92ccfa40578adcbdd67c618b8cf
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-14 00:06:08 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,30 +5,26 @@
  * @str- to append to.
  * return: dest.
  */
-char _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int s1 = 0;
+	int s2 =0;
 
-	i = 0;
-
-	j = 0;
-
-	while (*(dest + i) != '\0');
+	while (*(dest + s1) != '\0';
 	{
-		i++;
-		j++;
+
+		s1++;
 	}
 
-	i = 0;
+	while (s2 >= 0)
 
-	while (*(src + i) != '\0');
 	{
-		dest[j] = src[i];
+		*(dest + s1) = *(src + s2);
+		if (*(src + s2) == '\0')
+		break;
 
-		i++;
-		j++;
+		s1++;
+		s2++;
 	}
-	dest[j] = '\0';
-
 	return (dest);
 }
