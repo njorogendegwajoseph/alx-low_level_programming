e8db82faeb3ad496cb14ef12930d685b70c193a8
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-14 02:13:59 -0700

    Hello

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,7 @@ char *leet(char *);
 char *rot13(char *);
 int _putchar(char);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
