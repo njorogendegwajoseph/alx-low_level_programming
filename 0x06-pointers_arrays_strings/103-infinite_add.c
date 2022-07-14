2c10c3ba8174e975bdf1690628f1ad33b0793c23
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-14 06:08:25 -0700

    Hello

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,7 +13,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + c1) != '\0')
 		c1++;
-	while  (*(n2 + c2) != '\0')
+	while (*(n2 + c2) != '\0')
 		c2++;
 	if (c1 >= c2)
 		bg = c1;
@@ -22,26 +22,26 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
+	c1--,c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	while
 	{
 		op = dr1 + dr2 + add;
 		if (op >= 10)
 			add = op / 10;
 		else
 			add = 0;
-		if( op > 0)
+		if (op > 0)
 			*(r + bg) = (op % 10) + 48;
 		else 
 			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;i
-		else
+		if  (c1 > 0)
+			c1--, dr1 = *(n2 + c1) - 48;
+		else 
 			dr1 = 0;
 		if (c2 > 0)
 			c2--, dr2 = *(n2 + c2) - 48;
-		else
+		else 
 			dr2 = 0;
 		bg--, size_r--;
 	}
@@ -50,5 +50,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else 
 		return (r);
 }
-
 
