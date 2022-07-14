50a26d01526699fe08e243a4c4805485acb9efd9
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-14 00:33:13 -0700

    Hello

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strmp-string comparison
+ * @s1: initial string.
+ * @last string
+ * return: ) if correct.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = , j = 0;
+
+	while (j == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) =='\0'))
+			break;
+		j = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	
+	return (j);
+}
