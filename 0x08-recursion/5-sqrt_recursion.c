db5aea4441a7fdf16333397d809b7ec9ab0b4f87
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-20 02:06:55 -0700

    Hello

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,15 +10,12 @@
 int _evaluate(int i, int n)
 {
 	if (n == 0 || n == 1)
-	
 		return (n);
 	
 	else if (i * i < n)
-	
 		return (_evaluate(i + 1, n));
 	
 	else if (i * i == n)
-	
 		return (i);
 	return (-1);
 	return (-1);
