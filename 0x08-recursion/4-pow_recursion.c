e126ab66bc595903926e6593a11ad0d2d0f2858a
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-20 01:50:42 -0700

    Hello

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,11 +13,11 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if ( y != 0)
+	else if (y != 0)
 	{
-		return (x * _pow_recursion(x, y -1));
+		return (x * _pow_recursion(x, y - 1));
 	}
-	else 
+	else
 	{
 		return (1);
 	}
