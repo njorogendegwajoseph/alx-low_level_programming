7d4989eec8fed1e6d6f88330af832def2955cfd5
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-20 01:46:01 -0700

    Hello

---
+++ 0x08-recursion/4-power_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_rcursion - returns the value of x raised to y.
+ * @x :integer.
+ * @y integer.
+ * Return: Pow recursion.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0) /*Base*/
+	{
+		return (-1);
+	}
+	else if ( y != 0)
+	{
+		return (x * _pow_recursion(x, y -1));
+	}
+	else 
+	{
+		return (1);
+	}
+}
+
