a2daeb61a19b816c29d238fb1e21ff3b15561c85
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-07-25 04:57:13 -0700

    Hello

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * word_len - Locates the index marking the end of first word.
+ *
+ * @str - The string to be searched.
+ *
+ * Return - The index marking the end of the inital word.
+ *
+ */
+
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++:
+	}
+
+	return (len);
+}
+
+/**
+ * *count_words - Coint words in a string.
+ */
+
+int count_words(char *str)
+{
+	int index = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+	for (index = 0; index < len; index++)
+	{
+		if (*(str( + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+		return (words);
+	}
+
+	/**
+	 * str -split a string in 2.
+	 *
+	 */
+	char **strtow(char *str)
+	{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+	return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
+	}
+
