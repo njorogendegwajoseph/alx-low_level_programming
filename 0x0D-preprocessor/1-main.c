66069c57bd240316cd501a5062d778716c843659
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-01 05:25:23 -0700

    Hello

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,20 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main- Checke the code for students.
+ *
+ * Return - Rturn 0 when succesful.
+ */
+
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * File - PI.H;
+ *
+ * Description: The Macro name PI which will be replaced by the token 3.14159265359.
+ *
+ */
+
+#define PI 3.14159265359
+#endif
