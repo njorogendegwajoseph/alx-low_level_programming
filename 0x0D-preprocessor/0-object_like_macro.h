a32eea13b0910ecfc5281db1ad81f323d2cdc2bc
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-01 05:11:01 -0700

    Task done.

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJCT_LIKE_MACRO_H
+
+/**
+ * File: Object_like macro.h
+ * Desc: This Macro named SIZE and replace the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This repository is for learning the preprocessor. A rather useful feature in C.
