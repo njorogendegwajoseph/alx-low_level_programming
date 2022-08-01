b58174b4c3a4e0ffa7c7656eff08f3face5a3527
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-01 05:57:29 -0700

    Task done

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * fILE : 3- Function_like _macro.
+ * Description: This defines a function-lieke macro ABS(x) that computes the absolute value of an int.
+ *
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - checks the code for marking.
+ *
+ * return 0;
+ */
+
+int main(void)
+{
+	int i, j;
+
+
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return 0;
+}
+
