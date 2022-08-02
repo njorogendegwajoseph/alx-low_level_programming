4169503598c00513d028fcf5db2d61df729435a9
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-02 04:21:28 -0700

    Hello

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *mem = malloc(b);
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	if (mem ==NULL)
 		exit(98);
 
-	return (ptr);
+	return (mem);
 }
