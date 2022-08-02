9caebebeaf071b8702c6a1ed8b23543e8865c701
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-02 04:00:17 -0700

    Hello

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,12 +7,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d>owner);
-		free(d);
-	}
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
