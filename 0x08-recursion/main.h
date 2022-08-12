aeea55e4edc650e92d0b3586d420a07da607aac9
Joseph Njorroge <josephnjorogendegwa@gmail.com>
2022-08-12 06:34:01 -0700

    Hello

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
+#ifndef
+#define
+
 void _print_rev_recursion(char *s);
-int _putchar(char c);
 void _puts_recursions(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -8,4 +10,24 @@ int _evaluate(int i, int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);                                                                                                                  
+char *_strncat(char *dest, char *src, int n);                                                                                                          
+int _strcmp(char *s1, char *s2);                                                                                                                       
+void reverse_array(int *a, int n);                                                                                                                     
+char *leet(char *);                                                                                                                                    
+char *rot13(char *);                                                                                                                                   
+int _putchar(char);                                                                                                                                    
+void print_number(int n);                                                                                                                              
+char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                                           
+void print_buffer(char *b, int size);  
+char *_memset(char *s, char b, unsigned int n);                                                                                                        
+char *_memcpy(char *dest, char *src, unsigned int n);                                                                                                  
+char *_strchr(char *s, char c);                                                                                                                        
+unsigned int _strspn(char *s, char *accept);                                                                                                           
+char *_strpbrk(char *s, char *accept);                                                                                                                 
+char *_strstr(char *haystack, char *needle);                                                                                                           
+void print_chessboard(char (*a)[8]);                                                                                                                   
+int _putchar(char c);                                                                                                                                  
+void print_diagsums(int *a, int size);                                                                                                                 
 
+#endif
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,39 +1,67 @@
-int _putchar(char c);
+#ifndef                                                                                                                                                
 
-int _islower(int c);
+#define                                                                                                                                                
 
-int _isalpha(int c);
+                                                                                                                                                       
 
-int _abs(int n);
+void _print_rev_recursion(char *s);                                                                                                                    
 
-int _isupper(int c);
+void _puts_recursions(char *s);                                                                                                                        
 
-int _isdigit(int c);
+int _strlen_recursion(char *s);                                                                                                                        
 
-int _strlen(char *s);
+int factorial(int n);                                                                                                                                  
 
-void _puts(char *s);
+int _pow_recursion(int x, int y);                                                                                                                      
 
-char *_strcpy(char *dest, char *src);
+int _evaluate(int i, int n);                                                                                                                           
 
-int _atoi(char *s);
+int is_prime_number(int n);                                                                                                                            
 
-char *_strcat(char *dest, char *src);
+int is_palindrome(char *s);                                                                                                                            
 
-char *_strncat(char *dest, char *src, int n);
+int wildcmp(char *s1, char *s2);                                                                                                                       
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);                                                                                                                  
 
-int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);                                                                                                          
 
-char *_memset(char *s, char b, unsigned int n);
+int _strcmp(char *s1, char *s2);                                                                                                                       
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+void reverse_array(int *a, int n);                                                                                                                     
 
-char *_strchr(char *s, char c);
+char *leet(char *);                                                                                                                                    
 
-unsigned int _strspn(char *s, char *accept);
+char *rot13(char *);                                                                                                                                   
 
-char *_strpbrk(char *s, char *accept);
+int _putchar(char);                                                                                                                                    
 
-char *_strstr(char *haystack, char *needle);
+void print_number(int n);                                                                                                                              
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                                           
+
+void print_buffer(char *b, int size);                                                                                                                  
+
+char *_memset(char *s, char b, unsigned int n);                                                                                                        
+
+char *_memcpy(char *dest, char *src, unsigned int n);                                                                                                  
+
+char *_strchr(char *s, char c);                                                                                                                        
+
+unsigned int _strspn(char *s, char *accept);                                                                                                           
+
+char *_strpbrk(char *s, char *accept);                                                                                                                 
+
+char *_strstr(char *haystack, char *needle);                                                                                                           
+
+void print_chessboard(char (*a)[8]);                                                                                                                   
+
+int _putchar(char c);                                                                                                                                  
+
+void print_diagsums(int *a, int size);                                                                                                                 
+
+                                                                                                                                                       
+
+#endif                                                                                                                                                 
+
+
