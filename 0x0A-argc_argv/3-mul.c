036caaf30d4d2ae219b07ec5100ef601b2e9c2e6
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-13 13:48:16 +0300

    Corrections

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main_ multiplies two numbers
+* @argc: number of CL arguments.
+* @argv: array that contain the prgram CL args.
+* Return: 0 when success.
+*/
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Eroor\n");
+        return (1);
+    }
+    printf("%d\n", atoi(argv[i]) * atoi(argv[2]));
+    return (0);
+}
