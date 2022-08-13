1592d08d77bd2257c375b29fb628cd1d5251aaaf
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-13 13:22:54 +0300

    Some cute changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints its name, followed by a new line
+ @args - number of command line args.
+ @argv- array that contains the program command line args.
+ Return 0 on success.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Another day to work on being an engineer
