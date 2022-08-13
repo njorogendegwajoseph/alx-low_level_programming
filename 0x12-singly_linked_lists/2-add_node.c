391e4e9403d001699d20dbec4455fd78864e9f7a
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-13 10:22:23 +0300

    Corrections

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,18 +10,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    char *dup_str;
-    int len;
-
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    return (NULL);
-
-    dup_str = strdup(str);
-    if (dup_str == NULL)
+    list_t *newlist;
+    size_t nel;
+    newlist = malloc(sizeof(list_t));
+    if (newlist == NULL)
     {
-        free(new);
-        return (NULL);
+        return (NULL)
     }
+    newlist->str = strdup(str)
+    for (nel = 0; str[nel]; nel++)
+    ;
+    newlist->len = nel;
+    newlist->next = *head;
+    *head = newlist;
+    return (*head);
 }
