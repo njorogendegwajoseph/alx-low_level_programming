c915b72007bbb30155203d075a4614f24d1d7e5a
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-15 10:55:53 +0300

    Added task 2 and corrections, also header file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+* Desc: Header file containing declarations.
+*/
+
+#include <stdlib.h>
+
+/**
+* structlistint_s - singly linked lists
+* @n; integer
+* @next: points to the next node.
+*
+* Desc: singly linked list node structure
+*/
+
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+
+#endif /*LIST_H */
