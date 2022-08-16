74bb9872991b7c0a0057b5a722f8c0e0bf464e61
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-16 09:03:05 +0300

    Added some declarations in the header file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
 * Return: If the func fails - NULL
 */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
     listint_t *new, *copy = *head;
     unsigned int node;
