6b1c72bdc795ab19442339750fd5745f5d2c5329
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-08-22 12:51:44 +0300

    Added files

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,18 +2,17 @@
 #define HOLBERTON_H
 
 /**
-* Desc: Prototypes contained here.
-*
+* DEsc; Header files written in 0x14-file.
 */
 
 #include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/ stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, *text_content);
 
 #endif
