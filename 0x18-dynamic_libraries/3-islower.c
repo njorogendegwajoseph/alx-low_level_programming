bb7e626ad513009af0c7f8bb754957827d8ebc78
Joseph Njoroge <josephnjorogendegwa@gmail.com>
2022-10-04 13:15:59 +0300

    Added 0-main.c file

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - print sthe lower cas letters
+ * returns 1 when c is lower case
+ * returns 0 if otherwise
+ *
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
